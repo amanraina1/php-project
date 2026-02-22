e24c570b5fa7f4bf3787c7ed6c9d328c84879d11
amanraina1 <amanraina9086@gmail.com>
2026-02-22 16:45:27 +0530

    building a better router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Core\Router;
+
 const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . "Core/functions.php";
@@ -13,4 +15,12 @@ spl_autoload_register(function ($class) {
     require base_path("{$class}.php");
 });
 
-require base_path("Core/router.php");
+$router = new Router();
+//require base_path("Core/Router.php");
+$routes = require base_path("routes.php");
+
+// this function is used to remove the query parameters from the ur and only get the actual url
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = isset($_POST['_method']) ? $_POST['_method'] : $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);
